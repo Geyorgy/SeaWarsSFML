e402989e9645fd40d3469db02e7f5f30e492857d
Georgy <gosha_97@mail.ru>
2019-12-20 01:38:24 +0300

    Final version

---
+++ Battleship/src/Map.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Map {
+private:
+	short m_Rows = 10;
+	short m_Cols = 10;
+
+public:
+	void mapGenerate();
+	RectangleShape** m_Map;
+
+	Map();
+	~Map();
+};
---
+++ Battleship/src/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "Player.h"
+#include "Map.h"
+#include "PlayerAI.h"
+#include "Game.h"
+
+using namespace std;
+
+int main()
+{
+	short menuItem;
+
+	cout << "----------------------------- HELP -----------------------------" << endl;
+	cout << "The ships are placed in the next order:" << endl;
+	cout << "1. one 4-deck" << endl;
+	cout << "2. two 3-deck" << endl;
+	cout << "3. three 2-deck" << endl;
+	cout << "4. four 1-deck" << endl;
+	cout << "R - hide ships when they was placed" << endl;
+	cout << "Green deck - ship is wounded" << endl;
+	cout << "Red deck - ship is destroyed" << endl;
+	cout << "Purple deck - miss" << endl;
+
+	cout << "----------------------------- MENU --------------------------------" << endl;
+	cout << "1. Single player" << endl;
+	cout << "2. Two players" << endl;
+	cout << "3. Exit" << endl;
+	cout << "Your choice:";
+
+	do {
+		cin >> menuItem;
+		
+		if (!cin.fail() && (menuItem == 1 || menuItem == 2))
+		{
+			Game game(menuItem);
+			game.run();
+		}
+		else {
+			cout << "Invalid input" << endl;
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail());
+		}
+		
+	} while (menuItem != 3);
+
+	return 0;
+}
